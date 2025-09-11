c58ba5c520e0f6a56c2fee10dc57d6c4e73edc35
mmenanno <1358708+mmenanno@users.noreply.github.com>
2025-09-11 17:44:04 -0400

    fix(sudo): allow NIX_* env vars; drop -E in nixup rebuild

--- nix/modules/sudo.nix
+++ nix/modules/sudo.nix
@@ -4,9 +4,9 @@
   # Allow passwordless sudo for darwin-rebuild for the configured user
   environment.etc."sudoers.d/darwin-rebuild" = {
     text = ''
+      Defaults:${username} env_keep += "NIX_*"
       ${username} ALL=(root) NOPASSWD: /run/current-system/sw/bin/darwin-rebuild
     '';
-    mode = "0440";
   };
 }
 
